baf776899a7964db33b0b561bbaf28f2ca517424
beardedcactus <nscholl@usc.edu>
2018-05-07 17:33:35 -0700

    basic drain functionality

--- src/main/java/OceanMod/drainBlock/DrainBlock.java
+++ src/main/java/OceanMod/drainBlock/DrainBlock.java
@@ -1,15 +1,25 @@
 package oceanmod.drainBlock;
 
+import javax.annotation.Nullable;
+
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockDirectional;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumBlockRenderType;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class DrainBlock extends Block {
+public class DrainBlock extends BlockDirectional {
 
 	public DrainBlock(){
 		super(Material.ROCK);
@@ -22,4 +32,17 @@ public class DrainBlock extends Block {
     	return BlockRenderLayer.SOLID;
 	}
 	
+    
+    public void onBlockPlacedBy(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack)
+    {
+        //worldIn.setBlockState(pos, state.withProperty(FACING, EnumFacing.getDirectionFromEntityLiving(pos, placer)), 2);
+
+        if (!worldIn.isRemote)
+        {
+            if (worldIn.getBlockState(pos.down()).getMaterial() == Material.WATER){
+            	worldIn.setBlockToAir(pos.down());
+            }
+        }
+    }
+    
 }
